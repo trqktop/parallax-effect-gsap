8a8f67d5508883679f5be2f3817588b6ea536e0f
Shirin Pavel <trqktop@yandex.ru>
2023-01-20 11:43:59 +0300

    ghpages

--- src/constants/constants.js
+++
@@ -1,6 +0,0 @@
-const dropDown = document.querySelector('.dropdown')
-const dropdownAction = document.querySelector('.dropdown-action')
-const arrowDropDwon = document.querySelector('.header__menu-item-svg')
-const dropDownArray = Array.from(document.querySelectorAll('.dropdown__item'))
-const form = document.getElementById('form')
-export { dropDown, dropdownAction, arrowDropDwon, dropDownArray, form }
